7ca52076e8e308aa3fe60defee6f18292110e319
Junjie <90107547+junjieweb@users.noreply.github.com>
2022-05-26 16:40:18 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,8 @@ module.exports = {
     devServer: {//只用于开发环境
         proxy: {
             '/api': { // 只对请求路由以/api开头的请求进行代理转发
-                target: 'http://39.98.123.211', // 转发的目标url
+                // target: 'http://39.98.123.211', // 转发的目标url
+                target: 'http://sph-h5-api.atguigu.cn', // 转发的目标url
                 changeOrigin: true, // 支持跨域
                 // pathRewrite: {'^/api': ''} //后台接口都有/api
             }
